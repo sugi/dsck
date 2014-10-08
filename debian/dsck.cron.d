a364eb74c542e3adf6e15fd30412416beb1bfb2a
Tatsuki Sugiura <sugi@nemui.org>
2014-10-08 17:47:40 +0900

    Update debian files.

--- debian/dsck.cron.d
+++ debian/dsck.cron.d
@@ -1,6 +1,5 @@
 #
 # Regular cron jobs for the dsck package
 #
-PATH=/usr/local/bin:/usr/bin:/bin:/usr/local/sbin:/usr/sbin:/sbin
 
 0 */6	* * *	nobody	test -x /usr/sbin/dsck2mail && /usr/sbin/dsck2mail
